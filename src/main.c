654e6293d701e6b17a6ad41185ae17828766de89
Frenzied_Dog-C24 <hck134599@gmail.com>
2025-11-26 14:53:10 +0800

    State change test

--- src/main.c
+++ src/main.c
@@ -1,6 +1,7 @@
 #define RAYGUI_IMPLEMENTATION
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 #include "raylib.h"
 #include "raygui.h"
@@ -12,22 +13,30 @@ int main()
 	SetTargetFPS(60);
 
 	bool showMessageBox = false;
-
+	int state = 0;
 	while (!WindowShouldClose())
 	{
 		// Draw
 		//----------------------------------------------------------------------------------
 		BeginDrawing();
-		ClearBackground(GetColor(GuiGetStyle(DEFAULT, BACKGROUND_COLOR)));
 
-		if (GuiButton((Rectangle) { 24, 24, 120, 30 }, "#191#Show Message")) showMessageBox = true;
+		if (state == 0) {
+			SetWindowSize(400, 200);
+			ClearBackground(GetColor(GuiGetStyle(DEFAULT, BACKGROUND_COLOR)));
+			if (GuiButton((Rectangle) { 24, 24, 120, 30 }, "#191#Show Message")) showMessageBox = true;
 
-		if (showMessageBox)
-		{
-			int result = GuiMessageBox((Rectangle) { 85, 70, 250, 100 },
-				"#191#Message Box", "Hi! This is a message!", "Nice;Cool");
+			if (showMessageBox) {
+				int result = GuiMessageBox((Rectangle) { 85, 70, 250, 100 },
+					"#191#Message Box", "Hi! This is a message!", "Nice;Cool");
 
-			if (result >= 0) showMessageBox = false;
+				// printf("MessageBox result: %d\n", result);
+				if (result >= 0) showMessageBox = false;
+				if (result == 2) state = 1;
+			}
+		} else {
+			SetWindowSize(600, 400);
+			ClearBackground(GetColor(GuiGetStyle(DEFAULT, BACKGROUND_COLOR)));
+			if (GuiButton((Rectangle) { 80, 80, 120, 30 }, "#191#OWO")) state = 0;
 		}
 
 		EndDrawing();
