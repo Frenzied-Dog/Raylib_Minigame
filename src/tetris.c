dcaff3d4f7f69dfbde9f84130de88ca83f4d8a2b
Frenzied_Dog_mac <hck134599@gmail.com>
2025-12-11 16:23:15 +0800

    Funtion declaration

--- src/tetris.c
+++ src/tetris.c
@@ -2,19 +2,157 @@
 #include "raygui.h"
 #include "tetris.h"
 
-int TETRIS_WIDTH = 600;
-int TETRIS_HEIGHT = 400;
+static int WINDOW_WIDTH = 600;
+static int WINDOW_HEIGHT = 400;
+static TetrisState state = MENU;
+static int aniState = 0; // TODO: animation state
+
+static int board[TETRIS_BOARD_H][TETRIS_BOARD_W]; // 0=空，其它代表方塊種類
+static int score = 0;
+static int level = 1;
+static bool gameOver = false;
+static bool pause = false;
+
+static const Vector2 SHAPES[7][4][4] = {
+    // PIECE_I
+    {
+        { { -1, 0 }, { 0, 0 }, { 1, 0 }, { 2, 0 } },  // rot 0  ----
+        { { 1, -1 }, { 1, 0 }, { 1, 1 }, { 1, 2 } },  // rot 1  
+        { { -1, 1 }, { 0, 1 }, { 1, 1 }, { 2, 1 } },  // rot 2
+        { { 0, -1 }, { 0, 0 }, { 0, 1 }, { 0, 2 } },  // rot 3
+    },
+    // PIECE_O
+    {
+        { { 0, 0 }, { 1, 0 }, { 0, 1 }, { 1, 1 } },
+        { { 0, 0 }, { 1, 0 }, { 0, 1 }, { 1, 1 } },
+        { { 0, 0 }, { 1, 0 }, { 0, 1 }, { 1, 1 } },
+        { { 0, 0 }, { 1, 0 }, { 0, 1 }, { 1, 1 } },
+    },
+    // PIECE_T
+    {
+        { { -1, 0 }, { 0, 0 }, { 1, 0 }, { 0, 1 } },
+        { { 0, -1 }, { 0, 0 }, { 0, 1 }, { 1, 0 } },
+        { { -1, 0 }, { 0, 0 }, { 1, 0 }, { 0, -1 } },
+        { { 0, -1 }, { 0, 0 }, { 0, 1 }, { -1, 0 } },
+    },
+    // PIECE_S
+    {
+        { { 0, 0 }, { 1, 0 }, { -1, 1 }, { 0, 1 } },
+        { { 0, -1 }, { 0, 0 }, { 1, 0 }, { 1, 1 } },
+        { { 0, 0 }, { 1, 0 }, { -1, 1 }, { 0, 1 } },
+        { { 0, -1 }, { 0, 0 }, { 1, 0 }, { 1, 1 } },
+    },
+    // PIECE_Z
+    {
+        { { -1, 0 }, { 0, 0 }, { 0, 1 }, { 1, 1 } },
+        { { 1, -1 }, { 0, 0 }, { 1, 0 }, { 0, 1 } },
+        { { -1, 0 }, { 0, 0 }, { 0, 1 }, { 1, 1 } },
+        { { 1, -1 }, { 0, 0 }, { 1, 0 }, { 0, 1 } },
+    },
+    // PIECE_J
+    {
+        { { -1, 0 }, { 0, 0 }, { 1, 0 }, { -1, 1 } },
+        { { 0, -1 }, { 0, 0 }, { 0, 1 }, { 1, -1 } },
+        { { -1, 0 }, { 0, 0 }, { 1, 0 }, { 1, -1 } },
+        { { 0, -1 }, { 0, 0 }, { 0, 1 }, { -1, 1 } },
+    },
+    // PIECE_L
+    {
+        { { -1, 0 }, { 0, 0 }, { 1, 0 }, { 1, 1 } },
+        { { 0, -1 }, { 0, 0 }, { 0, 1 }, { 1, 1 } },
+        { { -1, -1 }, { -1, 0 }, { 0, 0 }, { 1, 0 } },
+        { { -1, -1 }, { 0, -1 }, { 0, 0 }, { 0, 1 } },
+    },
+};
+
 
 void tetris(menuState *mainState) {
-    SetWindowSize(TETRIS_WIDTH, TETRIS_HEIGHT);
+    SetWindowSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+    SetWindowPosition(180, 100);
     while (!WindowShouldClose() && *mainState == STATE_TETRIS) {
-        fixWindowDPI(TETRIS_WIDTH, TETRIS_HEIGHT);
-
+        fixWindowDPI(WINDOW_WIDTH, WINDOW_HEIGHT);
         BeginDrawing();
         ClearBackground(GetColor(GuiGetStyle(DEFAULT, BACKGROUND_COLOR)));
-        GuiLabel((Rectangle){ 200, 180, 200, 40 }, "Tetris Game Placeholder");
-        if (GuiButton((Rectangle) { 80, 80, 120, 30 }, "#191#Back to Menu"))
-            *mainState = MAIN_MENU;
+        switch(state) {
+        case MENU:
+            if (GuiButton((Rectangle) { 80, 90, 120, 30 }, "#191#Start Game"))
+                Tetris_Init();
+                state = PLAYING;
+            if (GuiButton((Rectangle) { 80, 50, 120, 30 }, "#191#Back to Menu"))
+                *mainState = MAIN_MENU;
+            break;
+        case PLAYING:
+            Draw_UI();
+            Draw_Board();
+
+            if (!gameOver) {
+                if (!pause) Tetris_Update(Tetris_GetInput());
+                else Draw_PauseScreen();
+            } else {
+                // TODO: Animation
+                state = GAMEOVER;
+            }
+            break;
+        case GAMEOVER:
+            Draw_UI();
+            Draw_Board();
+            // TODO: Game Over Screen
+            break;
+        }
+        
         EndDrawing();
     }
+}
+
+
+// TODO: Implement the functions below
+static void Tetris_Init() {
+    score = 0;
+    level = 1;
+    gameOver = false;
+    pause = false;
+}
+
+static TetrisInput Tetris_GetInput() {
+
+}
+
+static void Tetris_Update(TetrisInput input) {
+
+}
+
+static void Draw_UI() {
+
+}
+
+static void Draw_Board() {
+
+}
+
+static void Draw_PauseScreen() {
+
+}
+
+static PieceType random_piece(void) {
+
+}
+
+static void spawn_piece() {
+
+}
+
+static bool check_collision(const Piece* p) {
+
+}
+
+static void lock_piece() {
+
+}
+
+static int clear_lines() {
+
+}
+
+static void update_level_and_speed() {
+
 }
--- src/tetris.h
+++ src/tetris.h
@@ -1,10 +1,58 @@
-#pragma once
 #ifndef TETRIS_H
 #define TETRIS_H
 
 #include "common.h"
 
+#define TETRIS_BOARD_W 10
+#define TETRIS_BOARD_H 20
+
 void tetris(menuState* mainState);
+
+typedef enum {
+	PIECE_I = 0,
+	PIECE_O,
+	PIECE_T,
+	PIECE_S,
+	PIECE_Z,
+	PIECE_J,
+	PIECE_L,
+	PIECE_NONE
+} PieceType;
+
+typedef struct {
+	PieceType type;
+	int rotation;   // 0~3
+	int x;          // 在 board 上的 x (0 ~ TETRIS_BOARD_W-1)
+	int y;          // 在 board 上的 y (可以 <0 表示還在畫面上方)
+} Piece;
+
+typedef struct {
+	int move;           // -1: 左移一次, 1: 右移一次, 0: 不動
+	bool softDrop;      // true: 一格一格快速下落
+	bool hardDrop;      // true: 直接到底
+	bool rotateCW;      // 順時針旋轉
+	bool rotateCCW;     // 逆時針旋轉
+	bool hold;          // 是否使用 hold（如果你不想做 hold，可以忽略）
+} TetrisInput;
+
+typedef enum {
+	MENU,
+	PLAYING,
+	GAMEOVER
+} TetrisState;
+
+static void Tetris_Init();
+static TetrisInput Tetris_GetInput();
+static void Tetris_Update(TetrisInput input);
+static void Draw_UI();
+static void Draw_Board();
+static void Draw_PauseScreen();
+static PieceType random_piece(void);
+static void spawn_piece();
+static bool check_collision(const Piece* p);
+static void lock_piece();
+static int clear_lines();
+static void update_level_and_speed();
 
 
 #endif // TETRIS_H
